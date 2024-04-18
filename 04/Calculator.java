39e2d9ba47e0a878aa4310e9fde760ab38280f87
Abebe123000 <takashi123000@yahoo.co.jp>
2024-04-18 20:53:07 +0900

    add question

---
+++ 04/Calculator.java
@@ -0,0 +1,11 @@
+class Calculator {
+    public static void main(String[] args) {
+        int result = calculate();
+        System.out.println(result);
+    }
+
+    static double calculate() {
+        return 1 + 2 + 3 * 4 ;
+    }
+}
+
---
+++ 04/answer/Calculator.java
@@ -0,0 +1,12 @@
+class Calculator {
+    public static void main(String[] args) {
+        // int と double が一致していない
+        int result = calculate();
+        System.out.println(result);
+    }
+
+    static double calculate() {
+        return 1 + 2 + 3 * 4 ;
+    }
+}
+
